ebf41278242a90f7ec25b82e56eac95c91aeffeb
Ho-Xuan-Lam <61542100+Ho-Xuan-Lam@users.noreply.github.com>
2021-05-17 23:01:42 +0700

    first commit
    push

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oala5hjjZsecsXeZ5d6',
+        issuer: 'https://dev-65136284.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'],
+    }
+}
